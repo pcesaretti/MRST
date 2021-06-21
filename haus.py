8385b790b1c822f8955c599c90be984e02db4285
Paul Cesaretti <paul.cesaretti@gmail.com>
2021-06-21 13:51:33 -0400

    Add files via upload

--- haus.py
+++ haus.py
@@ -1,5 +1,6 @@
 import scipy.spatial as sp
 import numpy as np
+from numba import njit
 
 def distanceBetweenCurves(C1, C2):
     D = sp.distance.cdist(C1, C2, 'euclidean')
@@ -20,6 +21,33 @@ def distanceMatrixOfCurves(Curves):
         for j in range(i+1, numC):
             #print (i,j)
             D[i, j] = D[j, i] = distanceBetweenCurves(Curves[i], Curves[j])
+    return D
+
+@njit
+def distanceBetweenSurfaces(vol_a,vol_b):
+    dist_lst = np.empty(0)
+    for idx in range(len(vol_a)):
+        if(vol_a[idx][0] == -1):
+            break
+        dist_min = 100000.0        
+        for idx2 in range(len(vol_b)):
+            if(vol_b[idx2][0] == -1):
+                break
+            diff = np.array([vol_a[idx][0]-vol_b[idx2][0], vol_a[idx][1]-vol_b[idx2][1], vol_a[idx][2]-vol_b[idx2][2]])
+            dist= np.linalg.norm(diff)
+            if dist_min > dist:
+                dist_min = dist
+        dist_lst = np.append(dist_lst, dist_min)
+    return np.max(dist_lst)
+
+@njit
+def distanceMatrixOfSurfaces(Surfaces):
+    numC = len(Surfaces)
+
+    D = np.zeros((numC, numC))
+    for i in range(0, numC-1):
+        for j in range(i+1, numC):
+            D[i, j] = D[j, i] = distanceBetweenSurfaces(Surfaces[i], Surfaces[j])
     return D
 
 # def distance_on_sphere_numpy(coordinate_array):
